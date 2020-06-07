5574f0aa82759cc075e739a9b808d4aea0cdb755
IanMaguithi <maguithian13427@gmail.com>
2020-06-07 16:10:01 +0300

    modifications on models.py

--- project/customers/models.py
+++ project/customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ..retailers.models import Product
 
 
 # Create your models here.
@@ -11,3 +12,11 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+
+class Order(models.Model):
+
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.product.name
--- project/retailers/models.py
+++ project/retailers/models.py
@@ -22,18 +22,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Order(models.Model):
-    STATUS = (
-        ('Pending', 'Pending'),
-        ('Out for delivery', 'Out for delivery'),
-        ('Delivered', 'Delivered'),
-    )
-    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=500, null=True, choices=STATUS)
-    note = models.CharField(max_length=500, null=True)
-
-    def __str__(self):
-        return self.product.name
