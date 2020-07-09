4c4f1cbcdd5ffd1afd8525787e3046514b77d1a6
Alex Gathua <64085923+gathuaalex@users.noreply.github.com>
2020-07-10 01:22:28 +0300

    Update base.py

--- django-ecommerce-master/djecommerce/settings/base.py
+++ django-ecommerce-master/djecommerce/settings/base.py
@@ -99,8 +99,8 @@ AUTHENTICATION_BACKENDS = (
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='testpython301@gmail.com'
-EMAIL_HOST_PASSWORD='alex9122#'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
