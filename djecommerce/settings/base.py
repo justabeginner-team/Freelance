91ad79f5809a72f574b37bcabacff8ef39add5da
alex gathua <alexgathua3@gmail.com>
2020-08-06 19:24:43 +0300

    updated several modules

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -115,6 +115,13 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+
+#EMAIL_HOST = config('EMAIL_HOST')
+#EMAIL_PORT = config('EMAIL_PORT', cast=int)
+#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+#EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'TestSite Team <djangologinsmtp@gmail.com>'
